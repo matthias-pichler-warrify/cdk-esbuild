39b0854bcc863edb824112787efb4f689dc442d2
Momo Kornher <mail@moritzkornher.de>
2022-06-14 16:33:31 +1200

    chore: detect esbuild path with which package

--- src/esbuild-wrapper.ts
+++ src/esbuild-wrapper.ts
@@ -1,10 +1,24 @@
+import * as which from 'which';
+
 /* eslint-disable import/no-extraneous-dependencies */
 
 function esbuild() {
   try {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     return require('esbuild');
-  } catch (error) {
+  } catch (_one) {
+    /**
+     * esbuild is not installed. We fallback to use a (hopefully available)
+     * global binary and our bundled polyfill.
+     */
+    try {
+      if (!process.env?.ESBUILD_BINARY_PATH) {
+        process.env.ESBUILD_BINARY_PATH = which.sync('esbuild');
+      }
+    } catch (_two) {
+      throw new Error('The esbuild binary could not be found in path. Please follow the instructions to ensure esbuild is correctly installed.');
+    }
+
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     return require('./esbuild-polyfill');
   }
