9b6db949f79a269b86b33c86ea8fcfbdb883a7ac
Momo Kornher <mail@moritzkornher.de>
2022-06-23 15:53:30 +0100

    feat: find esbuild binary whithout which

--- src/esbuild-wrapper.ts
+++ src/esbuild-wrapper.ts
@@ -1,22 +1,16 @@
-import * as which from 'which';
-
 /* eslint-disable import/no-extraneous-dependencies */
 
 function esbuild() {
   try {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     return require('esbuild');
-  } catch (_one) {
+  } catch (_unused) {
     /**
      * esbuild is not installed. We fallback to use a (hopefully available)
      * global binary and our bundled polyfill.
      */
-    try {
-      if (!process.env?.ESBUILD_BINARY_PATH) {
-        process.env.ESBUILD_BINARY_PATH = which.sync('esbuild');
-      }
-    } catch (_two) {
-      throw new Error('The esbuild binary could not be found in path. Please follow the instructions to ensure esbuild is correctly installed.');
+    if (!process.env?.ESBUILD_BINARY_PATH) {
+      process.env.ESBUILD_BINARY_PATH = 'esbuild';
     }
 
     // eslint-disable-next-line @typescript-eslint/no-require-imports
